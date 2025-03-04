010b5404ea2ec123ffb9d0ff6012a49e98ea6a9f
pera <nikodijevicp01@gmail.com>
2025-03-04 18:45:08 +0100

    trying to change return address of indirect called fun

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
+#include "../lib/console.h"
+#include "../lib/hw.h"
 
+uint64 saved;
+
+void f() {
+    __putc('p');
+    __putc('\n');
+
+    __asm__ volatile("mv ra, %[retAddress]" : : [retAddress] "r" (saved));
+    __asm__ volatile("sd ra, 8(sp)");
+}
+
+void dispatch() {
+    __asm__ volatile("mv %[savedReg], ra" : [savedReg] "=r" (saved));
+    __asm__ volatile("mv ra, %[fAddress]" : : [fAddress] "r" (&f));
+}
 
 void main() {
-	
+    dispatch();
 }
