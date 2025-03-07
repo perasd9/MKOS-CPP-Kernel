381ca9accfe540b0a607511c96de18b6510fa3ce
pera <nikodijevicp01@gmail.com>
2025-03-07 15:00:58 +0100

    instance defitnion

--- src/MemoryAllocator.cpp
+++ src/MemoryAllocator.cpp
@@ -1,5 +1,12 @@
 #include "../h/MemoryAllocator.hpp"
 
+MemoryAllocator* MemoryAllocator::instance = nullptr;
+
+void MemoryAllocator::allocateMemory() {
+    
+}
+
+
 MemoryAllocator* MemoryAllocator::getInstance() {
     if (instance == nullptr) {
         allocateMemory();
@@ -7,4 +14,3 @@ MemoryAllocator* MemoryAllocator::getInstance() {
 
     return instance;
 }
-
