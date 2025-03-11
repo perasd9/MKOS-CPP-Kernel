b72bdbbae7b5eaed89bcdb31ff9625223d1b9e43
pera <nikodijevicp01@gmail.com>
2025-03-11 15:10:11 +0100

    added inline functions for controls and status register updating

--- h/riscV.hpp
+++ h/riscV.hpp
@@ -22,7 +22,7 @@ public:
 
     //supervisor trap value
     static uint64 read_stval();
-    static void write_stval(uint64 stvec);
+    static void write_stval(uint64 stval);
 
     //setting 1. 5. and 9. bit
     enum SipBitMask {
@@ -52,5 +52,84 @@ private:
     RiscV();
     ~RiscV();
 };
+
+//supervisor exception pc
+inline uint64 RiscV::read_sepc() {
+    uint64 sepc;
+
+    __asm__ volatile("csrr %[sepcPar], sepc" : [sepcPar] "=r" (sepc));
+
+    return sepc;
+}
+
+inline void RiscV::write_sepc(uint64 sepc) {
+    __asm__ volatile("csrw sepc, %[sepcPar]" : : [sepcPar] "r" (sepc));
+}
+
+//supervisor cause
+inline uint64 RiscV::read_scause() {
+    uint64 scause;
+
+    __asm__ volatile("csrr %[scausePar], scause" : [scausePar] "=r" (scause));
+
+    return scause;
+}
+
+inline void RiscV::write_scause(uint64 scause) {
+    __asm__ volatile("csrw scause, %[scausePar]" : : [scausePar] "r" (scause));
+}
+
+//supervisor trap vector
+inline uint64 RiscV::read_stvec() {
+    uint64 stvec;
+
+    __asm__ volatile("csrr %[stvecPar], stvec" : [stvecPar] "=r" (stvec));
+
+    return stvec;
+}
+
+inline void RiscV::write_stvec(uint64 stvec) {
+    __asm__ volatile("csrw stvec, %[stvecPar]" : : [stvecPar] "r" (stvec));
+}
+
+//supervisor trap value
+inline uint64 RiscV::read_stval() {
+    uint64 stval;
+
+    __asm__ volatile("csrr %[stvalPar], stval" : [stvalPar] "=r" (stval));
+
+    return stval;
+}
+
+inline void RiscV::write_stval(uint64 stval) {
+    __asm__ volatile("csrw stval, %[stvalPar]" : : [stvalPar] "r" (stval));
+}
+
+//supervisor interrupt pending
+inline uint64 RiscV::read_sip() {
+    uint64 sip;
+
+    __asm__ volatile("csrr %[sipPar], sip" : [sipPar] "=r" (sip));
+
+    return sip;
+}
+
+inline void RiscV::write_sip(uint64 sip) {
+    __asm__ volatile("csrw sip, %[sipPar]" : : [sipPar] "r" (sip));
+}
+
+//supervisor status
+inline uint64 RiscV::read_sstatus() {
+    uint64 sstatus;
+
+    __asm__ volatile("csrr %[sstatusPar], sstatus" : [sstatusPar] "=r" (sstatus));
+
+    return sstatus;
+}
+
+inline void RiscV::write_sstatus(uint64 sstatus) {
+    __asm__ volatile("csrw sstatus, %[sstatusPar]" : : [sstatusPar] "r" (sstatus));
+}
+
 
 #endif //RISCV_HPP
--- src/syscall_c.cpp
+++ src/syscall_c.cpp
@@ -1,7 +1,7 @@
 #include "../h/syscall_c.hpp"
 
 void* mem_alloc(size_t size) {
-
+    return nullptr;    
 }
 
 int mem_free(void *ptr) {
