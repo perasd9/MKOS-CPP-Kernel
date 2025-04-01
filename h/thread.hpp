34a2971fd16c1574ac8e7c24a825017da50bb925
pera <nikodijevicp01@gmail.com>
2025-04-01 14:01:44 +0200

    start expanding thread

--- h/thread.hpp
+++ h/thread.hpp
@@ -4,12 +4,31 @@
 class Thread {
 public:
 
+    using Body = void(*)(void*);
+
+    enum Status {
+        NEW,
+        READY,
+        FINISHED,
+        BLOCKED,
+        RUNNING
+    };
+
 private:
+
+    Thread(Body body, uint64* stack, uint64 timeSlice, void* arg);
 
     struct Context {
         uint64 ra;
         uint64 sp;
     };
+
+    Body body;
+    uint64 timeSlice;
+    uint64* stack;
+    Context ctx;
+    bool finished;
+
 
     static void contextSwitch(Context* oldCtx, Context* newCtx);
 
