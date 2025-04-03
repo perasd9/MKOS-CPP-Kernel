4e038d051f89308194ef90312d1c76967ee593d4
pera <nikodijevicp01@gmail.com>
2025-04-03 18:17:12 +0200

    plan thread yield execution

--- src/thread.cpp
+++ src/thread.cpp
@@ -11,6 +11,14 @@ Thread *Thread::createThread(Body body) {
 
 void Thread::yield() {
 
+    //need to be pushed registers on currently stack pointer of thread but i will omit that probably
+
+    //actually context switching
+    dispatch();
+
+    //popped registers for taking back at the return of last thread or coroutine for now
+    //coroutine is just independently control flow but that all will be thread
+    //that's the case why i named this thread
 }
 
 //currently being executed thread need to be changed with new thread from queue of ready threads
@@ -23,6 +31,6 @@ void Thread::dispatch() {
 
     running = Scheduler::getInstance()->get();
 
+    //actual context switching registers between 2 active threads
     contextSwitch(&old->ctx, &running->ctx);
 }
-
