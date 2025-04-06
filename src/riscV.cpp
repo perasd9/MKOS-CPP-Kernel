b4933e2fada24a7d05db66a30b49b9cbcc673e41
pera <nikodijevicp01@gmail.com>
2025-04-06 22:24:33 +0200

    started handling interrupt

--- src/riscV.cpp
+++ src/riscV.cpp
@@ -1,7 +1,20 @@
 #include "../h/riscV.hpp"
 
+#include "../lib/console.h"
+#include "../utils/printUtils.hpp"
+
 void RiscV::handleSupervisorTrap() {
-    uint64 scause = RiscV::read_scause();
+    uint64 scause = read_scause();
 
 
+    if (scause == 0x8000000000000001UL) { //interrupt, cause supervisor software interrupt
+        mclear_sip(SSIP);
+    } else if (scause == 0x8000000000000009UL) { //interrupt, cause supervisor external interrupt(console)
+        console_handler();
+    } else { //unexpected trap cause
+        printInt((int)scause);
+        printInt((int)read_sepc());
+        printInt((int)read_stval());
+    }
 }
+
