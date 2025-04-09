d546f9664bb66d83eb2dea8ade875cf5ed7ed321
pera <nikodijevicp01@gmail.com>
2025-04-09 20:34:54 +0200

    started sycalls specification extending for c api

--- h/syscall_c.hpp
+++ h/syscall_c.hpp
@@ -5,6 +5,21 @@
 
 // memory management functions
 void* mem_alloc(size_t size);
+
 int mem_free(void* ptr);
+
+//thread syscalls c
+class Thread;
+typedef Thread* thread_t;
+
+int thread_create(thread_t* handle, void(*start_routine)(void*), void* arg);
+
+int thread_exit();
+
+void thread_dispatch();
+
+//semaphore syscalls c need to be filled later
+
+//console syscalls c for getc and putc need to be filled later
 
 #endif //SYSCALL_C_HPP
