542c3a673667a3339d2a1827d3e9940171786531
pera <nikodijevicp01@gmail.com>
2025-04-18 20:32:24 +0200

    expanding codes

--- src/riscV.cpp
+++ src/riscV.cpp
@@ -6,43 +6,93 @@
 
 void RiscV::popSppSpie() {
     __asm__ volatile("csrw sepc, ra");
-    
-    //potentially mc status
-
+    mclear_sstatus(SPP);
     __asm__ volatile("sret");
 }
 
 void RiscV::handleSupervisorTrap() {
     uint64 scause = read_scause();
-    
+
     uint64 a0;
 
     __asm__ volatile("mv %0, a0" : "=r" (a0));
 
     if (scause == 0x8000000000000001UL) { //interrupt, cause supervisor software interrupt
-
-        Thread::timeSliceCounter++;
-
-        if (Thread::timeSliceCounter >= Thread::running->get_time_slice()) {
-            uint64 sepc = read_sepc();
-            uint64 sstatus = read_sstatus();
-
-            Thread::timeSliceCounter = 0;
-            Thread::dispatch();
-
-            write_sepc(sepc);
-            write_sstatus(sstatus);
-        }
-
         mclear_sip(SSIP);
     } else if (scause == 0x8000000000000009UL) { //interrupt, cause supervisor external interrupt(console)
         console_handler();
     } else if (scause == 0x0000000000000008UL || scause == 0x0000000000000009UL) {
         //not interrupt, cause is environment call from user mode of supervisor mode
 
-        switch(){
+        switch(scause){
+            case 0x92:
+                uint64 sepc = read_sepc() + 4;
+                uint64 sstatus = read_sstatus();
 
-       }
+                Thread::timeSliceCounter = 0;
+                Thread::dispatch();
+
+                write_sepc(sepc);
+                write_sstatus(sstatus);
+                break;
+
+            /*case 0x01:
+
+                break;
+
+            case 0x02:
+
+                break;
+
+            case 0x11:
+
+                break;
+
+            case 0x12:
+
+                break;
+
+            case 0x13:
+
+                break;
+
+            case 0x21:
+
+                break;
+
+            case 0x22:
+
+                break;
+
+            case 0x23:
+
+                break;
+
+            case 0x24:
+
+                break;
+
+            case 0x25:
+
+                break;
+
+            case 0x26:
+
+                break;
+
+            case 0x31:
+
+                break;
+
+            case 0x41:
+
+                break;
+
+            case 0x42:
+
+                break;*/
+
+        }
     } else { //unexpected trap cause
         printInt((int)scause);
         printInt((int)read_sepc());
