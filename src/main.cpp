cdf8bbf87599efccf11139fca7a6189bebc35734
pera <nikodijevicp01@gmail.com>
2025-04-21 22:21:49 +0200

    extending

--- src/main.cpp
+++ src/main.cpp
@@ -34,36 +34,35 @@ void workerB(void * par) {
 }
 
 void main() {
-     //dispatch cannot be tested with thread execution
-     //dispatch();
-     char s[] = "\n";
-     char num[] = "999";
-     char buf[3];
-     printString((char *)&HEAP_START_ADDR);
-     printInt((int)(size_t)HEAP_START_ADDR);
-     printString(s);
-     printInt((int)(size_t)HEAP_END_ADDR);
-     printInt(stringToInt(num));
-    
-     getString(buf, 3);
-     printString(buf);
+    //dispatch();
+    // char s[] = "\n";
+    // char num[] = "999";
+    // char buf[3];
+    // printString((char *)&HEAP_START_ADDR);
+    // printInt((int)(size_t)HEAP_START_ADDR);
+    // printString(s);
+    // printInt((int)(size_t)HEAP_END_ADDR);
+    // printInt(stringToInt(num));
+    //
+    // getString(buf, 3);
+    // printString(buf);
 
-     RiscV::write_stvec((size_t)&RiscV::supervisorTrap);
-     __asm__ volatile("ecall");
+    // RiscV::write_stvec((size_t)&RiscV::supervisorTrap);
+    // __asm__ volatile("ecall");
 
-    //Thread* threads[3];
+    Thread* threads[3];
 
-    //threads[0] = Thread::createThread(nullptr);
+    threads[0] = Thread::createThread(nullptr);
 
-    //Thread::running = threads[0];
+    Thread::running = threads[0];
 
-    //threads[1] = Thread::createThread(workerA);
+    threads[1] = Thread::createThread(workerA);
 
-    //threads[2] = Thread::createThread(workerB);
+    threads[2] = Thread::createThread(workerB);
 
-   // while (threads[1]->is_finished() == false && threads[2]->is_finished() == false)
-       // Thread::yield();
+    while (threads[1]->isFinished() == false && threads[2]->isFinished() == false)
+        Thread::yield();
 
-    //for (auto &thread : threads)
-        //delete thread;
+    for (auto &thread : threads)
+        delete thread;
 }
