2d770074dfbd33739354d2ba20c0984107eae771
pera <nikodijevicp01@gmail.com>
2025-04-29 22:19:04 +0200

    mem alloc and free right way for forwarding to interrupt handler

--- src/riscV.cpp
+++ src/riscV.cpp
@@ -11,93 +11,73 @@ void RiscV::popSppSpie() {
 }
 
 void RiscV::handleSupervisorTrap() {
-    uint64 scause = read_scause();
-
     uint64 a0;
+    uint64 a1;
 
     __asm__ volatile("mv %0, a0" : "=r" (a0));
+    uint64 scause = read_scause();
 
-    if (scause == 0x8000000000000001UL) { //interrupt, cause supervisor software interrupt
+
+    if (scause == 0x8000000000000001UL) {
+        //interrupt, cause supervisor software interrupt
         mclear_sip(SSIP);
-    } else if (scause == 0x8000000000000009UL) { //interrupt, cause supervisor external interrupt(console)
+    } else if (scause == 0x8000000000000009UL) {
+        //interrupt, cause supervisor external interrupt(console)
         console_handler();
     } else if (scause == 0x0000000000000008UL || scause == 0x0000000000000009UL) {
         //not interrupt, cause is environment call from user mode of supervisor mode
-        printInt(a0);
 
-        switch(a0){
-            case 0x09:
+        if (a0 == 0x92) {
+            uint64 sepc = read_sepc() + 4;
+            uint64 sstatus = read_sstatus();
 
-                uint64 sepc = read_sepc() + 4;
-                uint64 sstatus = read_sstatus();
+            Thread::timeSliceCounter = 0;
+            Thread::dispatch();
 
-                Thread::timeSliceCounter = 0;
-                Thread::dispatch();
-
-                write_sepc(sepc);
-                write_sstatus(sstatus);
-                break;
-
-            /*case 0x01:
-
-                break;
-
-            case 0x02:
-
-                break;
-
-            case 0x11:
-
-                break;
-
-            case 0x12:
-
-                break;
-
-            case 0x13:
-
-                break;
-
-            case 0x21:
-
-                break;
-
-            case 0x22:
-
-                break;
-
-            case 0x23:
-
-                break;
-
-            case 0x24:
-
-                break;
-
-            case 0x25:
-
-                break;
-
-            case 0x26:
-
-                break;
-
-            case 0x31:
-
-                break;
-
-            case 0x41:
-
-                break;
-
-            case 0x42:
-
-                break;*/
-
+            write_sepc(sepc);
+            write_sstatus(sstatus);
         }
-    } else { //unexpected trap cause
-        printInt((int)scause);
-        printInt((int)read_sepc());
-        printInt((int)read_stval());
+        else if (a0 == 0x01) { //mem_alloc code sys call
+            uint64 sepc = read_sepc() + 4;
+            uint64 sstatus = read_sstatus();
+
+            __asm__ volatile("mv %0, x11" : "=r" (a1));
+
+            // __asm__ volatile("ld a0, 11*8(fp)");
+            // __asm__ volatile("mv %0, a0" : "=r" (a1));
+
+            uint64 addr;
+            addr = (uint64) MemoryAllocator::getInstance()->mem_alloc(a1 * MEM_BLOCK_SIZE);
+
+            // __asm__ volatile("mv a0, %0" : : "r" (addr));
+            __asm__ volatile("sd %0, 10*8(fp)" : : "r" (addr));
+
+            write_sepc(sepc);
+            write_sstatus(sstatus);
+        }
+        else if (a0 == 0x02) { //mem_free code sys call
+            uint64 sepc = read_sepc() + 4;
+            uint64 sstatus = read_sstatus();
+
+            __asm__ volatile("mv %0, x11" : "=r" (a1));
+            int ret = MemoryAllocator::getInstance()->mem_free((void *) a1);
+
+            __asm__ volatile("sd %0, 10*8(fp)" : : "r" (ret));
+
+            write_sepc(sepc);
+            write_sstatus(sstatus);
+        }
+
+    } else {
+        //unexpected trap cause
+        char s[] = "\n";
+        char s1[] = "  ";
+        printInt(scause);
+        printString(s1);
+        printInt(read_sepc());
+        printString(s1);
+        printInt(read_stval());
+        printString(s);
     }
+
 }
