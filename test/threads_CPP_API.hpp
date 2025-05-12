d9a933a962881700098d753339308e3893ec02f0
pera <nikodijevicp01@gmail.com>
2025-05-12 20:38:32 +0200

    threads cpp api tests work

--- test/threads_CPP_API.hpp
+++ test/threads_CPP_API.hpp
@@ -87,3 +87,88 @@ void WorkerB::workerBodyB(void* arg) {
     finishedB = true;
     thread_dispatch();
 }
+
+void WorkerC::workerBodyC(void* arg) {
+    char* s;
+
+    uint8 i = 0;
+    for (; i < 3; i++) {
+        s = (char*)"C: i="; printString(s); printInt(i); s = (char*)"\n"; printString(s);
+    }
+
+    s = (char*)"C: dispatch\n"; printString(s);
+    __asm__ ("li t1, 7");
+    thread_dispatch();
+
+    uint64 t1 = 0;
+    __asm__ ("mv %[t1], t1" : [t1] "=r"(t1));
+
+    s = (char*)"C: t1="; printString(s); printInt(t1); s = (char*)"\n"; printString(s);
+
+    uint64 result = fibonacci(12);
+    s = (char*)"C: fibonaci="; printString(s); printInt(result); s = (char*)"\n"; printString(s);
+
+    for (; i < 6; i++) {
+        s = (char*)"C: i="; printString(s); printInt(i); s = (char*)"\n"; printString(s);
+    }
+
+    s = (char*)"C finished!\n"; printString(s);
+    finishedC = true;
+    thread_dispatch();
+}
+
+void WorkerD::workerBodyD(void* arg) {
+    char* s;
+
+    uint8 i = 10;
+    for (; i < 13; i++) {
+        s = (char*)"D: i="; printString(s); printInt(i); s = (char*)"\n"; printString(s);
+    }
+
+    s = (char*)"D: dispatch\n"; printString(s);
+    __asm__ ("li t1, 5");
+    thread_dispatch();
+
+    uint64 result = fibonacci(16);
+
+    s = (char*)"D: fibonaci="; printString(s); printInt(result); s = (char*)"\n"; printString(s);
+
+    for (; i < 16; i++) {
+        s = (char*)"D: i="; printString(s); printInt(i); s = (char*)"\n"; printString(s);
+    }
+
+    s = (char*)"D finished!\n"; printString(s);
+    finishedD = true;
+    thread_dispatch();
+}
+
+
+void threads_CPP_API_test() {
+    char* s;
+
+    Thread* threads[4];
+
+    threads[0] = new WorkerA();
+    s = (char*)"ThreadA created\n"; printString(s);
+
+    threads[1] = new WorkerB();
+    s = (char*)"ThreadB created\n"; printString(s);
+
+    threads[2] = new WorkerC();
+    s = (char*)"ThreadC created\n"; printString(s);
+
+    threads[3] = new WorkerD();
+    s = (char*)"ThreadD created\n"; printString(s);
+
+    for(int i=0; i<4; i++) {
+        threads[i]->start();
+    }
+
+    while (!(finishedA && finishedB && finishedC && finishedD)) {
+        Thread::dispatch();
+    }
+
+    for (auto thread: threads) { delete thread; }
+}
+
+#endif //THREADS_CPP_API_HPP
