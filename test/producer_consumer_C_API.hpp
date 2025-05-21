4b5c6c85f9b811554694151cda7e6db24485dad5
pera <nikodijevicp01@gmail.com>
2025-05-21 20:34:36 +0200

    c api produces consumer tests finished

--- test/producer_consumer_C_API.hpp
+++ test/producer_consumer_C_API.hpp
@@ -50,3 +50,95 @@ void producer(void *arg) {
 
     sem_signal(data->wait);
 }
+
+void consumer(void *arg) {
+    struct thread_data *data = (struct thread_data *) arg;
+
+    int i = 0;
+    while (!threadEnd) {
+        int key = data->buffer->get();
+        i++;
+
+        putc(key);
+
+        if (i % (5 * data->id) == 0) {
+            thread_dispatch();
+        }
+
+        if (i % 80 == 0) {
+            putc('\n');
+        }
+    }
+
+    while (data->buffer->getCnt() > 0) {
+        int key = data->buffer->get();
+        putc(key);
+    }
+
+    sem_signal(data->wait);
+}
+
+void producerConsumer_C_API() {
+    char* s;
+    char input[30];
+    int n, threadNum;
+
+    s = (char*)"Unesite broj proizvodjaca?\n"; printString(s);
+    getString(input, 30);
+    threadNum = stringToInt(input);
+
+    s = (char*)"Unesite velicinu bafera?\n"; printString(s);
+    getString(input, 30);
+    n = stringToInt(input);
+
+    s = (char*)"Broj proizvodjaca "; printString(s); printInt(threadNum);
+    s = (char*)" i velicina bafera "; printString(s); printInt(n);
+    s = (char*) ".\n"; printString(s);
+
+    if(threadNum > n) {
+        s = (char*) "Broj proizvodjaca ne sme biti manji od velicine bafera!\n"; printString(s);
+
+        return;
+    } else if (threadNum < 1) {
+        s = (char*) "Broj proizvodjaca mora biti veci od nula!\n"; printString(s);
+
+        return;
+    }
+
+    auto buffer = new Buffer(n);
+
+    sem_open(&waitForAll, 0);
+
+    thread_t threads[threadNum];
+    thread_t consumerThread;
+
+    struct thread_data data[threadNum + 1];
+
+    data[threadNum].id = threadNum;
+    data[threadNum].buffer = buffer;
+    data[threadNum].wait = waitForAll;
+    thread_create(&consumerThread, consumer, data + threadNum);
+
+    for (int i = 0; i < threadNum; i++) {
+        data[i].id = i;
+        data[i].buffer = buffer;
+        data[i].wait = waitForAll;
+
+        thread_create(threads + i,
+                      i > 0 ? producer : producerKeyboard,
+                      data + i);
+    }
+
+    thread_dispatch();
+
+    for (int i = 0; i <= threadNum; i++) {
+        sem_wait(waitForAll);
+    }
+
+    sem_close(waitForAll);
+
+    delete buffer;
+
+}
+
+#endif //PRODUCER_CONSUMER_C_API_HPP
