42acba8613360960f2420237b7aed821aecd150c
pera <nikodijevicp01@gmail.com>
2025-06-01 15:11:33 +0200

    producer consumer cpp sync api tests started

---
+++ test/producer_consumer_CPP_API_Sync.hpp
@@ -0,0 +1,48 @@
+#ifndef PRODUCER_CONSUMER_CPP_API_SYNC_HPP
+#define PRODUCER_CONSUMER_CPP_API_SYNC_HPP
+
+#include "buffer_CPP_API.hpp"
+#include "../h/syscall_cpp.hpp"
+#include "../utils/printUtils.hpp"
+
+Semaphore* waitForAll;
+
+struct thread_data {
+    int id;
+    BufferCPP *buffer;
+    Semaphore* wait;
+};
+
+volatile int threadEnd = 0;
+
+class ProducerKeyboard:public Thread {
+    thread_data* td;
+    void producerKeyboard(void *arg);
+public:
+    ProducerKeyboard(thread_data* _td):Thread(), td(_td) {}
+
+    void run() override {
+        producerKeyboard(td);
+    }
+};
+
+void ProducerKeyboard::producerKeyboard(void *arg) {
+    struct thread_data *data = (struct thread_data *) arg;
+
+    int key;
+    int i = 0;
+    while ((key = getc()) != 0x1b) {
+        data->buffer->put(key);
+        i++;
+
+        if (i % (10 * data->id) == 0) {
+            Thread::dispatch();
+        }
+    }
+
+    threadEnd = 1;
+    td->buffer->put('!');
+
+    data->wait->signal();
+}
+
